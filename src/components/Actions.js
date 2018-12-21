5630d3a620c51d62cd7dadd0e1a1eb9eb673540b
Tim Douglas <tim.douglas@phocassoftware.com>
2018-12-21 23:04:17 +1100

    Basic conditional CSS

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,11 +1,19 @@
-// TODO: Conditional CSS classes to disable/enable buttons
+// TODO: TIDY Conditional CSS classes to disable/enable buttons
 
 const copy = require("copy-text-to-clipboard");
 
 const actions = function createActionsComponent(fastn) {
   return fastn(
     "div",
-    fastn("button", "COPY").on("click", (event, scope) => {
+    fastn(
+      "button",
+      {
+        class: fastn.binding("results", function(results) {
+          return !results ? "blbb" : "quiggle";
+        })
+      },
+      "COPY"
+    ).on("click", (event, scope) => {
       var text = document.querySelector(".issue-summary").innerText;
       copy(text);
     }),
